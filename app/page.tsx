1ccc166c325b2294496635c137517c8aa8bd8179
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2025-02-11 16:33:57 +0530

    testing a cd 2

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs so here I have added the workflows
           </a>
         </div>
       </main>
