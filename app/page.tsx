c03617b19bb614effce45f06d2c7da30b019a400
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2025-02-12 11:47:15 +0530

    tesing on cd on main domain

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside capitalize list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside text-7xl capitalize list-decimal text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             this is farhan
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
